2cf5f507efec64ad24d4e53ab0a14d467a944bd9
Prasanth Kumar <pakumar@gmail.com>
2016-01-16 12:43:13 -0800

    Use sum function

--- hello.c
+++ hello.c
@@ -3,7 +3,8 @@ int square(int x);
 int main(int argc, char **argv)
 {
     printf("Hello World\n");
-    printf("Square of %d is %d\n", 10, square(10 * 10));
+    printf("Square of %d is %d\n", 10, square(10));
+    printf("Sum of %d and %d is %d\n", 10, 20, sum(10, 20));
     return 0;
 }
 
