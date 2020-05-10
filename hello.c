59cb935e2bc752e6502565609cd7c76aa7adc2bd
Prasanth Kumar <pakumar@gmail.com>
2020-05-09 20:44:13 -0700

    Add max()

--- hello.c
+++ hello.c
@@ -5,6 +5,7 @@ int main(int argc, char **argv)
     printf("Hello World\n");
     printf("Square of %d is %d\n", 10, square(10));
     printf("Sum of %d and %d is %d\n", 10, 20, sum(10, 20));
+    printf("Max of %d and %d is %d\n", 10, 20, max(10, 20));
     return 0;
 }
 
@@ -16,5 +17,14 @@ int square(int x)
 int sum(int x, int y)
 {
     return x + y;
+}
+
+int max(int x, int y)
+{
+    if x > y {
+        return x;
+    }
+
+    return y;
 }
 
